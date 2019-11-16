a3523cf26df58d0ec3f3fca3e8dac857a105bbbe
mihail <oldschool3105@gmail.com>
2019-11-16 22:31:31 +0200

    Commit With NearHundred

--- src/DisplayFunc.java
+++ src/DisplayFunc.java
@@ -8,9 +8,11 @@ public class DisplayFunc {
    public static Random number = new Random();
    public static final int numbRandOne = number.nextInt(100);
    public static final int numbRandTwo = number.nextInt(100);
+    public static final int numbRandThree = number.nextInt(100);
 
     public static void main(String[] args) {
             System.out.println("Результат сложения " + Diff21.diffResolution(numbRandOne));
+        System.out.println("Near Hundred " + NearHundred.nearHundredSolution(numbRandOne));
         }
 
 
