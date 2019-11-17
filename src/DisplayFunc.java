d3697bd13c3dadd2d0363b284f6691bbe9e27c81
mihail <oldschool3105@gmail.com>
2019-11-17 02:49:47 +0200

    Commit with task MissingChar

--- src/DisplayFunc.java
+++ src/DisplayFunc.java
@@ -6,16 +6,18 @@ public class DisplayFunc {
     public static Random number = new Random();
     public static final int numbRandOne = number.nextInt(100);
     public static final int numbRandTwo = number.nextInt(100);
-    public static final int numbRandThree = number.nextInt(100);
-
+    public static final int numbRandThree = number.nextInt(5);
     public static Random randomweakday = new Random();
     public static final boolean weakday = randomweakday.nextBoolean();
     public static final boolean vacation = randomweakday.nextBoolean();
+    public static final String word = "qwertyuiop";
 
     public static void main(String[] args) {
         System.out.println("Результат сложения " + Diff21.diffResolution(numbRandOne));
         System.out.println("Near Hundred " + NearHundred.nearHundredSolution(numbRandOne));
         SleepIn.sleapInInfo();
+        System.out.println("Text with missing simbol " + MissingChar.misingCharResolution(word, numbRandThree));
+
     }
 
 
